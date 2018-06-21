8ab8e36c0facb923aea26c07a9209540fd988205
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-21 00:33:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 # dog.rb
 class Dog
-  def initialize(name, breed="Mutt")
+  def initialize(name)
     @name = name
-    @breed = breed
   end
 
   def name=(name)
@@ -11,14 +10,9 @@ class Dog
   def name
     @name
   end
-def breed=(breed)
-  @breed = breed
-end
-def breed
-  @breed
-end
+
 end
 
-fido = Dog.new("Fido", "Pug")
+fido = Dog.new("Fido")
 fido.name
-fido.breed
+
